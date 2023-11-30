5cdc262ca368cd334082f38ece8277a6d136a977
linuxgamingcentral <101075966+linuxgamingcentral@users.noreply.github.com>
2023-11-30 01:37:41 -0500

    Update install_gcadapter-oc-kmod.sh

--- install_gcadapter-oc-kmod.sh
+++ install_gcadapter-oc-kmod.sh
@@ -1,26 +1,61 @@
 #!/bin/bash
 
+# if a password was set by decky, this will run when the program closes
+temp_pass_cleanup() {
+  echo $PASS | sudo -S -k passwd -d deck
+}
+
+# removes unhelpful GTK warnings
+zen_nospam() {
+  zenity 2> >(grep -v 'Gtk' >&2) "$@"
+}
+
+# check if JQ is installed
+if ! command -v jq &> /dev/null
+then
+    echo "JQ could not be found, please install it"
+    echo "Info on how to install it can be found at https://stedolan.github.io/jq/download/"
+    exit 1
+fi
+
+# check if github.com is reachable
+if ! curl -Is https://github.com | head -1 | grep 200 > /dev/null
+then
+    echo "Github appears to be unreachable, you may not be connected to the internet"
+    exit 1
+fi
+
 # if the script is not root yet, get the password and rerun as root
 if (( $EUID != 0 )); then
     PASS_STATUS=$(passwd -S deck 2> /dev/null)
+    if [ "$PASS_STATUS" = "" ]; then
+        echo "Deck user not found. Continuing anyway, as it probably just means user is on a non-steamos system."
+    fi
 
-    if [ "${PASS_STATUS:5:2}" = "NP" ]; then # if no password is set, exit
-        if ( zen_nospam --title="GC Adapter OC Kmod Installer" --width=300 --height=200 --question --text="No root password set. Please set up a root password before installation by running passwd from a terminal." ); then
-        exit 1; fi
+    if [ "${PASS_STATUS:5:2}" = "NP" ]; then # if no password is set
+        if ( zen_nospam --title="GC Adapter OC Kmod" --width=300 --height=200 --question --text="You appear to have not set an admin password.\nDecky can still install by temporarily setting your password to 'Decky!' and continuing, then removing it when the installer finishes\nAre you okay with that?" ); then
+            yes "Decky!" | passwd deck # set password to Decky!
+            trap temp_pass_cleanup EXIT # make sure that password is removed when application closes
+            PASS="Decky!"
+        else exit 1; fi
     else
         # get password
         FINISHED="false"
         while [ "$FINISHED" != "true" ]; do
-            PASS=$(zen_nospam --title="GC Adapter OC Kmod Installer" --width=300 --height=100 --entry --hide-text --text="Enter your sudo/admin password")
+            PASS=$(zen_nospam --title="GC Adapter OC Kmod" --width=300 --height=100 --entry --hide-text --text="Enter your sudo/admin password")
             if [[ $? -eq 1 ]] || [[ $? -eq 5 ]]; then
                 exit 1
             fi
             if ( echo "$PASS" | sudo -S -k true ); then
                 FINISHED="true"
             else
-                zen_nospam --title="GC Adapter OC Kmod Installer" --width=150 --height=40 --info --text "Incorrect Password"
+                zen_nospam --title="GC Adapter OC Kmod" --width=150 --height=40 --info --text "Incorrect Password"
             fi
         done
+    fi
+
+    if ! [ $USER = "deck" ]; then
+        zen_nospam --title="GC Adapter OC Kmod" --width=300 --height=100 --warning --text "You appear to not be on a deck.\nDecky should still mostly work, but you may not get full functionality."
     fi
     
     echo "$PASS" | sudo -S -k bash "$0" "$@" # rerun script as root
