17055f6bf9d64481fbb363a12842425cc3d0a844
linuxgamingcentral <linuxgamingcentral@pm.me>
2024-01-05 02:58:27 -0500

    add option to uninstall if module already exists

--- install_gcadapter-oc-kmod.sh
+++ install_gcadapter-oc-kmod.sh
@@ -1,13 +1,70 @@
 #!/bin/bash
 
-# If a password was set by the program, this will run when the program closes
-temp_pass_cleanup() {
-  echo $PASS | sudo -S -k passwd -d deck
-}
+clear
+
+echo -e "GCC Adapter Overclocking script by Linux Gaming Central.\n"
+sleep 1
+
+title="GC Adapter OC Kmod"
 
 # Removes unhelpful GTK warnings
 zen_nospam() {
   zenity 2> >(grep -v 'Gtk' >&2) "$@"
+}
+
+info() {
+	text=$1
+	zen_nospam --title="$title" --width=300 --height=100 --info --text "$1"
+}
+
+question() {
+	text=$1
+	zen_nospam --title="$title" --width=300 --height=200 --question --text="$1"
+}
+
+install_overclock_module() {
+	echo -e "Cloning repo...\n"
+	sleep 1
+	git clone https://github.com/hannesmann/gcadapter-oc-kmod.git
+	cd gcadapter-oc-kmod
+
+	echo -e "Compiling...\n"
+	sleep 1
+	make
+
+	echo -e "Installing  module...\n"
+	sleep 1
+	sudo -Sp '' insmod gcadapter_oc.ko <<<${sudo_password}
+
+	echo -e "Persisting across reboots...\n"
+	sleep 1
+	sudo mkdir -p "/usr/lib/modules/$(uname -r)/extra"
+	sudo cp gcadapter_oc.ko "/usr/lib/modules/$(uname -r)/extra"
+	sudo depmod
+	echo "gcadapter_oc" | sudo tee /etc/modules-load.d/gcadapter_oc.conf
+	
+	echo -e "Success!\n"
+	sleep 1
+}
+
+uninstall() {
+	cd gcadapter-oc-kmod/
+	
+	echo -e "Removing module...\n"
+	sleep 1
+	sudo -Sp '' rmmod gcadapter_oc.ko <<<${sudo_password}
+	
+	echo -e "Cleaning up...\n"
+	sleep 1
+	make clean
+	
+	echo -e "Deleting configuration...\n"
+	sleep 1
+	sudo rm /usr/lib/modules/$(uname -r)/extra/gcadapter_oc.ko
+	sudo rm /etc/modules-load.d/gcadapter_oc.conf
+	
+	echo -e "Success!\n"
+	sleep 1
 }
 
 # Check if GitHub is reachable
@@ -17,70 +74,86 @@ then
     exit 1
 fi
 
-# If the script is not root yet, get the password and re-run as root
-if (( $EUID != 0 )); then
-    PASS_STATUS=$(passwd -S deck 2> /dev/null)
-    if [ "$PASS_STATUS" = "" ]; then
-        echo "Warning: Deck user not found!"
-    fi
-
-    if [ "${PASS_STATUS:5:2}" = "NP" ]; then # if no password is set, set up a temporary password to Smash!
-        if ( zen_nospam --title="GC Adapter OC Kmod" --width=300 --height=200 --question --text="You appear to have not set an admin password.\nGC Adapter OC Kmod can still install by temporarily setting your password to 'Smash!' and continuing, then removing it when the installer finishes\nAre you okay with that?" ); then
-            yes "Smash!" | passwd deck # set password to Smash!
-            trap temp_pass_cleanup EXIT # make sure that password is removed when application closes
-            PASS="Smash!"
-        else exit 1; fi
-    else
-        # get password
-        FINISHED="false"
-        while [ "$FINISHED" != "true" ]; do
-            PASS=$(zen_nospam --title="GC Adapter OC Kmod" --width=300 --height=100 --entry --hide-text --text="Enter your sudo/admin password")
-            if [[ $? -eq 1 ]] || [[ $? -eq 5 ]]; then
-                exit 1
-            fi
-            if ( echo "$PASS" | sudo -S -k true ); then
-                FINISHED="true"
-            else
-                zen_nospam --title="GC Adapter OC Kmod" --width=150 --height=40 --info --text "Incorrect password!"
-            fi
-        done
-    fi
-
-    if ! [ $USER = "deck" ]; then # check if the user is on Deck. If not, provide a warning.
-        zen_nospam --title="GC Adapter OC Kmod" --width=300 --height=100 --warning --text "Error: you're likely not using a Steam Deck. Please note this installer will only work said device."
-    fi
-    
-    echo "$PASS" | sudo -S -k bash "$0" "$@" # rerun script as root
-    exit 1
+# check to see if user is using SteamOS. If not, provide a warning
+if [ $USER == "deck" ]; then
+	echo -e "User is using SteamOS.\n"
+else
+	info "You're using a distro that isn't supported by this program. The program will continue to operate, but you may get issues along the way."
 fi
 
-# Disable the filesystem until we're done
-sudo steamos-readonly disable
+# get sudo password
+sudo_password=$(zen_nospam --password --title="$title")
+if [[ ${?} != 0 || -z ${sudo_password} ]]; then
+	echo -e "User canceled.\n"
+	exit
+elif ! sudo -kSp '' [ 1 ] <<<${sudo_password} 2>/dev/null; then
+	echo -e "User entered wrong password.\n"
+	info "Wrong password."
+	exit
+else
+	echo -e "Password entered, let's proceed...\n"
+	sleep 1
+fi
 
-# Get pacman keys
-sudo pacman-key --init
-sudo pacman-key --populate archlinux holo
+mkdir -p $HOME/Applications
+cd $HOME/Applications
 
-# Install kernel headers and dev tools
-sudo pacman -S --needed --noconfirm base-devel "$(cat /usr/lib/modules/$(uname -r)/pkgbase)-headers"
+# check to see if overclock module already exists. If it does, offer the user to uninstall
+if [ -f /usr/lib/modules/$(uname -r)/extra/gcadapter_oc.ko ]; then
+	if ( question "Overclock module already installed. Would you like to uninstall?" ); then
+	yes |
+		if [ $USER == "deck" ]; then
+			echo -e "Disabling read-only file system...\n"
+			sleep 1
+			sudo -Sp '' steamos-readonly disable <<<${sudo_password}
+			echo -e "Unlocked!\n"
+			sleep 1
+			
+			uninstall
+			
+			echo -e "Restoring read-only file system...\n"
+			sleep 1
+			sudo steamos-readonly enable
+			echo -e "Restored!\n"
+			sleep 1
+							
+			info "Overclock module removed!"
+		else
+			uninstall
+			
+			info "Overclock module removed!"
+		fi
+	else
+		exit
+	fi
+else
+	# overclock on Deck
+	if [ $USER == "deck" ]; then
+		echo -e "Disabling read-only file system...\n"
+		sleep 1
+		sudo -Sp '' steamos-readonly disable <<<${sudo_password}
 
-# Clone repo and change into the directory
-git clone https://github.com/hannesmann/gcadapter-oc-kmod.git
-cd gcadapter-oc-kmod
+		echo -e "Populating pacman keys...\n"
+		sleep 1
+		sudo pacman-key --init
+		sudo pacman-key --populate archlinux holo
 
-# Make the module
-make
+		echo -e "Installing base development tools and Linux headers...\n"
+		sleep 1
+		sudo pacman -S --needed --noconfirm base-devel "$(cat /usr/lib/modules/$(uname -r)/pkgbase)-headers"
+		
+		install_overclock_module
 
-# Install it
-sudo insmod gcadapter_oc.ko
+		echo -e "Restoring read-only file system...\n"
+		sleep 1
+		sudo steamos-readonly enable
 
-# Persist across reboots
-sudo mkdir -p "/usr/lib/modules/$(uname -r)/extra"
-sudo cp gcadapter_oc.ko "/usr/lib/modules/$(uname -r)/extra"
-sudo depmod
-echo "gcadapter_oc" | sudo tee /etc/modules-load.d/gcadapter_oc.conf
-
-# Lock the filesystem back up
-sudo steamos-readonly enable
-
-zen_nospam --title="GC Adapter OC Kmod" --width=300 --height=100 --info --text "Installation successful!"
+		info "Installation complete!"
+		exit
+	# other distros
+	else
+		install_overclock_module
+		info "Installation complete!"
+		exit
+	fi
+fi
